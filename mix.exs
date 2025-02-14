7c194f3fa6c51c25364d1c71f61933fe20f04ea5
selenil <abergesdesouza@gmail.com>
2025-02-14 01:27:42 -0500

    add mix_gleam and gleam libraries

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Lissome.MixProject do
   use Mix.Project
+  @app :lissome
 
   def project do
     [
-      app: :lissome,
+      app: @app,
+      name: "#{@app}",
       version: "0.1.0",
       elixir: "~> 1.18",
+      archives: [mix_gleam: "~> 0.6"],
+      compilers: [:gleam] ++ Mix.compilers(),
+      erlc_paths: ["build/dev/erlang/#{@app}/_gleam_artefacts"],
+      erlc_include_path: "build/dev/erlang/#{@app}/include",
+      prune_code_paths: false,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -21,8 +29,16 @@ defmodule Lissome.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # gleam dependencies
+      {:gleam_stdlib, "~> 0.34 or ~> 1.0"},
+      {:gleeunit, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:lustre, "~> 4.6.3"}
+    ]
+  end
+
+  defp aliases do
+    [
+      "deps.get": ["deps.get", "gleam.deps.get"]
     ]
   end
 end
--- test/lissome_test.exs
+++ test/lissome_test.exs
@@ -5,4 +5,12 @@ defmodule LissomeTest do
   test "greets the world" do
     assert Lissome.hello() == :world
   end
+
+  test "can call Gleam code" do
+    assert :lissome.hello() == "Hello from Gleam!"
+  end
+
+  test "can call Gleam library" do
+    assert :gleam@string.length("Hello") == 5
+  end
 end
