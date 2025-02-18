31f7d1ba634c40c6d1a70d6a9a54db3c10e8b053
selenil <abergesdesouza@gmail.com>
2025-02-18 02:13:01 -0500

    add project metadata

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule Lissome.MixProject do
   use Mix.Project
+
   @app :lissome
+  @version "0.1.0"
+
+  @repo_url "https://github.com/selenil/lissome"
 
   def project do
     [
       app: @app,
-      name: "#{@app}",
-      version: "0.1.0",
+      name: "Lissome",
+      version: @version,
       elixir: "~> 1.18",
       archives: [mix_gleam: "~> 0.6"],
       compilers: [:gleam] ++ Mix.compilers(),
@@ -15,7 +19,23 @@ defmodule Lissome.MixProject do
       prune_code_paths: false,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+
+      # Hex
+      description: description(),
+      package: package(),
+
+      # Docs
+      docs: [
+        source_ref: "v#{@version}",
+        source_url: @repo_url,
+        homepage_url: @repo_url,
+        main: "readme",
+        extras: ["README.md"],
+        links: %{
+          "GitHub" => @repo_url
+        }
+      ]
     ]
   end
 
@@ -45,6 +65,22 @@ defmodule Lissome.MixProject do
   defp aliases do
     [
       "deps.get": ["deps.get", "gleam.deps.get"]
+    ]
+  end
+
+  defp description do
+    "Integration of Gleam's Lustre framework with Phoenix LiveView"
+  end
+
+  defp package do
+    [
+      maintainers: [""],
+      licenses: ["MIT"],
+      links: %{
+        Changelog: @repo_url <> "/blob/main/CHANGELOG.md",
+        GitHub: @repo_url
+      },
+      files: ~w(lib mix.exs .formatter.exs LICENSE.md README.md CHANGELOG.md)
     ]
   end
 end
