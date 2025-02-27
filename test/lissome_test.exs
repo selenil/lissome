1db4a58b9cf5a87213d870c5d16c630bbe6bb84c
selenil <abergesdesouza@gmail.com>
2025-02-27 00:58:25 -0500

    clean gleam src

--- test/lissome_test.exs
+++ test/lissome_test.exs
@@ -1,12 +1,4 @@
 defmodule LissomeTest do
   use ExUnit.Case
   doctest Lissome
-
-  test "can call Gleam code" do
-    assert :lissome.hello() == "Hello from Gleam!"
-  end
-
-  test "can call Gleam library" do
-    assert :gleam@string.length("Hello") == 5
-  end
 end
