bb21299491ef509068d7d189821254854ebe276a
selenil <abergesdesouza@gmail.com>
2025-02-27 00:48:14 -0500

    remove unnecessary dependencies

--- mix.exs
+++ mix.exs
@@ -42,10 +42,6 @@ defmodule Lissome.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:esbuild, "~> 0.9"},
-      {:phoenix, "~> 1.7.18"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.0.0"},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
 
