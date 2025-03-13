69e20f7fea20f7013ea8663d3a1fcf26769bf070
selenil <abergesdesouza@gmail.com>
2025-03-13 13:31:00 -0400

    Add tests to check if the gleam package is available in erlang.

--- test/lissome_test.exs
+++ test/lissome_test.exs
@@ -5,4 +5,40 @@ defmodule LissomeTest do
   test "can call Gleam code" do
     assert :gleam@list.reverse([1, 2, 3]) == [3, 2, 1]
   end
+
+  test "can call code from the Lissome Gleam package" do
+    assert Code.ensure_loaded?(:lissome)
+    assert Code.ensure_loaded?(:lissome@live_view)
+
+    assert :lissome.get_flags("mock-id", "mock-decoder") == {:error, :not_a_browser}
+
+    push_event_result =
+      :lissome@live_view.push_event(
+        "mock-hook",
+        "mock-event",
+        nil,
+        fn _ -> nil end
+      )
+
+    push_event_to_result =
+      :lissome@live_view.push_event_to(
+        "mock-hook",
+        "mock-query-selector",
+        "mock-event",
+        nil,
+        fn _ -> nil end
+      )
+
+    handle_event_result =
+      :lissome@live_view.handle_event(
+        "mock-hook",
+        "mock-event",
+        fn _ -> nil end
+      )
+
+    assert Enum.all?(
+             [push_event_result, push_event_to_result, handle_event_result],
+             &(is_tuple(&1) && tuple_size(&1) == 2 && elem(&1, 0) == :effect)
+           )
+  end
 end
