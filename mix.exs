9daf74531060e76238fe74a98d543fdbd1b56b07
selenil <abergesdesouza@gmail.com>
2025-04-01 16:39:00 -0400

    fix deps issue

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Lissome.MixProject do
   defp deps do
     [
       {:phoenix_live_view, "~> 1.0.0"},
-      {:gleam_stdlib, "~> 0.44", app: false},
+      {:gleam_stdlib, ">= 0.53.0 and < 2.0.0", app: false},
       {:lustre, "~> 4.6.3", app: false},
       {:file_system, "~> 0.3 or ~> 1.0", optional: true},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false}
